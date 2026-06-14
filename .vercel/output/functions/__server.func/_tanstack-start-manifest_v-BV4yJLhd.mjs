e1443861e1d542f5d817b79dfbf405730ce9e0c3
AryanShrest <aryanshrestha0307@gmail.com>
2026-06-14 17:02:26 +0545

    fix: remove .vercel/output from git and add to .gitignore

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-BV4yJLhd.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "d:/kathmandu-clean-pros/src/routes/__root.tsx", children: ["/", "/admin", "/services/$id"], preloads: ["/assets/index-DJZS06jV.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-DJZS06jV.js" } }] } } });
-export {
-  tsrStartManifest
-};
